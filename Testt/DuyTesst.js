ac4694f0c89c93df2b575b785741235138f77294
nhatduy2210 <tenduyhole@gmail.com>
2023-10-01 00:30:36 +0700

    DuyTesstlan1

---
+++ Testt/DuyTesst.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const DuyTesst = () => {
+  return (
+    <View>
+      <Text>DuyTesst</Text>
+    </View>
+  )
+}
+
+export default DuyTesst
+
+const styles = StyleSheet.create({})
